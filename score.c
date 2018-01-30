fb325101e3e11aa64d44ef881109a42b2d22ae8d
alexandre laloy <l2info020@ic2s124-18.univ-lemans.fr>
2018-01-30 15:30:23 +0100

    fonction score

---
+++ score.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 8
+
+/********************************************************************************************
+Prend en compte la matrice et la couleur du joueur, puis renvoie le nb de pions de sa couleur
+********************************************************************************************/
+
+int score(char mat[N][N], char couleur){
+
+	int i, j;
+	int cpt = 0;
+
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			if(mat[i][j] == couleur){
+				cpt++;
+			}
+		}
+	}
+	return cpt;
+}
