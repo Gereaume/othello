aa5e7e3939c9103a61a57ca45b125b94873d6b38
alexandre laloy <l2info020@ic2s114-16.univ-lemans.fr>
2018-02-13 16:24:13 +0100

    v1 coupPoss

---
+++ Bac_a_sable_Alexandre/coupPoss.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void copie(char mat[N][N], char mat2[N][N]);
+
+int retourner(mat[N][N], char couleur, int lig, int col);
+
+int coupPoss(char mat[N][N], char mat2[N][N], char couleur, int lig, int col);
