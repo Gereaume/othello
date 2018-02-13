8546f925a61794f138e032466c9e57a1115effc9
jerome le_querrec <l2info021@ic2s114-17.univ-lemans.fr>
2018-02-13 15:10:23 +0100

    minor

--- Commun/Test_des_fonctions/Gestion_tour.c
+++ Commun/Test_des_fonctions/Gestion_tour.c
@@ -3,43 +3,76 @@
 	
 #include "gestion_matrice.c"
 
+
+/******************************************************************************************
+Cette fonction permet de savoir qui a gagné
+******************************************************************************************/
+
+int gagnant(char mat[N][N]){
+	int nb_noir = 0, nb_blanc = 0;				//Déclaration des variables				
+
+	nb_noir = compter_pion(mat,noir);			//On compte le nombre de pion noir
+	nb_blanc = compter_pion(mat,blanc);			//On compte le nombre de pion blanc
+	
+	if(nb_noir>nb_blanc)					//Si il y a plus de pion noir on return 1 donc le joueur n°1 a gagné
+		return 1
+	else							//Si il y a plus de pion blanc on return donc le joueur n°2 a gagné
+		return 0
+
+}
+
+
+
+/******************************************************************************************
+Cette fonction permet le bon déroulement du tour des différents joueurs
+******************************************************************************************/
 int gestion_tour(){
-	int i = 0;
+
+
+	int i = 0,nb_noir = 0, nb_blanc = 0;			//Déclaration des variables
 	char noir = "n";
 	char blanc = "b";
 	while(coup_possible())
 	{	
 		if((i%2)!=1)
 		{
-			system("clear");
-			printf("\tTour du joueur n°1\n");
-			lire_mat(mat);
-			printf("\tVeuillez saisir les coordonnées ou vous souhaitez jouer");
-			scanf("%i%i",&lig,&col);
-			while(!coup_possible())
+			system("clear");			//On nettoie le terminal au debut du tour
+			printf("\tTour du joueur n°1\n");	//On indique que c'est le tour du joueur n°1
+			lire_mat(mat);				//On affiche la matrice
+			printf("\n\tVeuillez saisir les coordonnées ou vous souhaitez jouer");
+			scanf("%i%i",&lig,&col);		//Le joueur saisie les coordonnées où il veut jouer
+			while(!coup_possible())			//Tant que le coup est impossible le joueur doit ressaisir les coordonnées de l'endroit ou il veut jouer
 			{
-				printf("\tCe coup la n'est pas possible ");
+				printf("\n\tCe coup la n'est pas possible ");
 				scanf("%i%i",&lig,&col);
 			}
-			ecrire_mat(mat,noir,lig,col);
-			lire_mat(mat)
-			i+=1;
+			ecrire_mat(mat,noir,lig,col);		//Une fois que l'endroit ou le joueur a décider de jouer est possible on l'ecrit dans la matrice
+			lire_mat(mat);				//On affiche la matrice une fois le coup fait
+			i+=1;					//On incrémente i
 		}		
 		else if((i%1)=1)
 		{
-			system("clear");
-			printf("\tTour du joueur n°2\n");
-			lire_mat(mat);
-			printf("\tVeuillez saisir les coordonnées ou vous souhaitez jouer");
-			scanf("%i%i",&lig,&col);
-			while(!coup_possible())
+			system("clear");			//On nettoie le terminal au debut du tour
+			printf("\tTour du joueur n°2\n");	//On indique que c'est le tour du joueur n°2
+			lire_mat(mat);				//On affiche la matrice
+			printf("\n\tVeuillez saisir les coordonnées ou vous souhaitez jouer");
+			scanf("%i%i",&lig,&col);		//Le joueur saisie les coordonnées où il veut jouer
+			while(!coup_possible())			//Tant que le coup est impossible le joueur doit ressaisir les coordonnées de l'endroit ou il veut jouer
 			{
-				printf("\tCe coup la n'est pas possible ");
+				printf("\n\tCe coup la n'est pas possible ");
 				scanf("%i%i",&lig,&col);
 			}
-			ecrire_mat(mat,blanc,lig,col);
-			lire_mat(mat)
-			i+=1
+			ecrire_mat(mat,blanc,lig,col);		//Une fois que l'endroit ou le joueur a décider de jouer est possible on l'ecrit dans la matrice
+			lire_mat(mat);				//On affiche la matrice une fois le coup fait
+			i+=1;					//On incrémente i
 		}
 	}
+	nb_noir = compter_pion(mat,noir);
+	nb_blanc = compter_pion(mat,blanc);
+	
+	if(gagnant == 1)
+		printf("\tC'est le joueur n°1 qui a gagné\n");
+	else
+		prinft("\tC'est le joueur n°2 qui a gagné\n");
+
 }
