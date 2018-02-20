5be15de4cebad41e1bed18a20a514f47e16a0cdc
Laloy Alexandre <l2info020@ic2s120-15.univ-lemans.fr>
2018-02-20 15:07:46 +0100

    minor

--- Bac_a_sable_Alexandre/coupPoss.c
+++ Bac_a_sable_Alexandre/coupPoss.c
@@ -1,10 +1,25 @@
+/**
+*\file coupPoss.c
+*\brief Ce fichier contient des fonctions permettant de savoir si un coup est possible et cb de pions sont retournés
+*\author Alexandre Laloy
+*\version 1.0
+*\date 20 fevrier 2018
+**/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
 #include "coupPoss.h"
+#include "gestion_matrice.h"
+#include "define.h"
 
 
+/**
+*Copie une matrice dans une autre
+**/
 void copie(char mat[N][N], char mat2[N][N]){
 
 	int i, j;
@@ -17,12 +32,23 @@ void copie(char mat[N][N], char mat2[N][N]){
 }
 
 
-int retourner(mat[N][N], char couleur, int lig, int col){
 
-	int i, j, cpt;
+/**
+*Renvoie le nombre de pions retourné, si aucun retourné, return 0
+**/
+int retourner(char mat[N][N], char couleur, int lig, int col){
+
+	int i, j, cpt,x;
 	int cpt_final = 0;
 
-	if(mat[lig-1][col-1] != '*' && mat[lig-1][col-1] != couleur){
+
+
+
+/*********************************************************************************
+Il y a 8 fois ce traitement pour les 8 directions possibles pour retourner un pion
+*********************************************************************************/
+
+	if(mat[lig-1][col-1] != '*' && mat[lig-1][col-1] != couleur){	/** Si la case est de la couleur de l'adversaire **/
 		i = lig-1;
 		j = col-1;
 		cpt = 0;
@@ -31,14 +57,14 @@ int retourner(mat[N][N], char couleur, int lig, int col){
 			j--;
 			cpt++;
 		}
-		if(mat[i][j] == couleur){
+		if(mat[i][j] == couleur){	/** Si la case est de la couleur du joueur, alors on retourne les pions précédemment compté **/
 			i = lig-1;
 			j = col-1;
 			for(x=0;x<cpt;x++){
 				mat[i][j] = couleur;
 				i--;
 				j--;
-				cpt_final++;
+				cpt_final++;	/** On incrémente le compteur final **/
 			}
 		}
 	}
@@ -190,23 +216,27 @@ int retourner(mat[N][N], char couleur, int lig, int col){
 		}
 	}
 
-	return cpt_final;
+	return cpt_final;	/** On renvoie le nombre de pions retournés **/
 }
 
 
 
-/******************************************
-Renvoie 1 si le coup est possible, sinon 0
-******************************************/
-
+/**
+*Renvoie 1 si le coup est possible, sinon 0
+**/
 int coupPoss(char mat[N][N], char mat2[N][N], char couleur, int lig, int col){
 	int nb_pions_ret;
 	copie(mat, mat2); // copie contenu de mat dans mat2
 
 	if((mat[lig][col] == '*') && !hors_mat(lig,col)){ // si les coordonnées correspondent à une zone vide
 		nb_pions_ret = retourner(mat2, couleur, lig, col); // si le pion posé retourne au moins 1 pion
-		printf("");
-		return 1;	
+		if(nb_pions_ret != 0){
+			printf("Le nb retourner est de %i\n", nb_pions_ret);
+			return 1;
+		}
+		else{
+			return 0;
+		}
 	}
 	else{
 		return 0; // sinon faux
