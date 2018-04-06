4212d71de4b0cc5c323de40b67ee05049a3d6607
julienlr <35669075+julienlr@users.noreply.github.com>
2018-04-06 18:22:17 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,113 +1,34 @@
 /**
-*\file min_max_ordi.c
-*\brief Ce fichier contient le main permettant de jouer une partie d'othello
-*\author Alexandre Laloy
-*\version 1.0
-*\date 16 mars 2018
+*\file affichage.c
+*\brief Fichier qui lance le jeu de l'othello
+*\author Julien Le Roux
+*\version 2.0
+*\date 06 avril 2018
 **/
 
-#include <stdio.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-
-#include "gestion_matrice.h"
-#include "gestion_tour.h"
+#include <time.h>
 #include "define.h"
-#include "joueur.h"
+#include "sdljeu.h"
 
 
 
-/**
-*\brief Le main permet de jouer à l'othello, en mode humain vs humain, ou contre l'ordinateur avec différents niveaux
-**/
-int main(){
+int main(int argc, char** argv)
+{
 
-	t_joueur joueur1;
-	t_joueur joueur2;
-	int mat[N][N];
-	int choix;
+	SDL_Window* fenetre = NULL;  /** pointeur création fenetre SDL **/ 
 
-    do{
-        printf("\nBienvenue au Jeu de l'Othello\n");
-        printf("\nMenu :\n");
-        printf(" 1 - Jouer mode Humain VS Humain\n");
-        printf(" 2 - Jouer mode Humain VS Ordi (Easy)\n");
-        printf(" 3 - Jouer mode Humain VS Ordi (Medium)\n");
-        printf(" 4 - Jouer mode Humain VS Ordi (Hard)\n");
-        printf(" 5 - Jouer mode Humain VS Ordi (Impossible)\n");
-        printf(" 6 - Quitter\n");
-        printf("Votre choix  : ");
-        scanf("%i",&choix);
+	SDL_Renderer* renderer = NULL; /** creation du renderer 	**/
+	
+	aff_menu(renderer, fenetre, 0); /** appel du menu 	**/
 
-        switch(choix){
-            case 1 : 	init_mat(mat);
-            			printf("\n\tJoueur n°1 entrez votre pseudo : ");
-						scanf("%s%*c",joueur1.nom_joueur);
-						printf("\n\tJoueur n°2 entrez votre pseudo : ");
-						scanf("%s%*c",joueur2.nom_joueur);
-						gestion_tour(mat,joueur1,joueur2); break;
+	return 0;
 
-            case 2 : 	init_mat(mat);
-            			/*
-            			joueur2.nom_joueur[50] = "Ordi";
-            			*/
-            			joueur2.nom_joueur[0] = 'O';
-            			joueur2.nom_joueur[1] = 'r';
-            			joueur2.nom_joueur[2] = 'd';
-            			joueur2.nom_joueur[3] = 'i';
-            			joueur2.nom_joueur[4] = '\0';
-            			
-            			printf("\n\tJoueur entrez votre pseudo : ");
-						scanf("%s%*c",joueur1.nom_joueur);
-						gestion_tour_ia(mat,joueur1,joueur2, 3); break;
-
-			case 3 : 	init_mat(mat);
-            			/*
-            			joueur2.nom_joueur[50] = "Ordi";
-            			*/
-            			joueur2.nom_joueur[0] = 'O';
-            			joueur2.nom_joueur[1] = 'r';
-            			joueur2.nom_joueur[2] = 'd';
-            			joueur2.nom_joueur[3] = 'i';
-            			joueur2.nom_joueur[4] = '\0';
-            			
-            			printf("\n\tJoueur entrez votre pseudo : ");
-						scanf("%s%*c",joueur1.nom_joueur);
-						gestion_tour_ia(mat,joueur1,joueur2, 5); break;
-
-			case 4 : 	init_mat(mat);
-            			/*
-            			joueur2.nom_joueur[50] = "Ordi";
-            			*/
-            			joueur2.nom_joueur[0] = 'O';
-            			joueur2.nom_joueur[1] = 'r';
-            			joueur2.nom_joueur[2] = 'd';
-            			joueur2.nom_joueur[3] = 'i';
-            			joueur2.nom_joueur[4] = '\0';
-            			
-            			printf("\n\tJoueur entrez votre pseudo : ");
-						scanf("%s%*c",joueur1.nom_joueur);
-						gestion_tour_ia(mat,joueur1,joueur2, 7); break;
-
-			case 5 : 	init_mat(mat);
-            			/*
-            			joueur2.nom_joueur = "Ordi";
-            			*/
-            			joueur2.nom_joueur[0] = 'O';
-            			joueur2.nom_joueur[1] = 'r';
-            			joueur2.nom_joueur[2] = 'd';
-            			joueur2.nom_joueur[3] = 'i';
-            			joueur2.nom_joueur[4] = '\0';
-            			
-            			printf("\n\tJoueur entrez votre pseudo : ");
-						scanf("%s%*c",joueur1.nom_joueur);
-						gestion_tour_ia(mat,joueur1,joueur2, (N*N-4)); break;
-						
-            case 6 : break;
-            default: printf("Erreur : votre choix doit être compris entre 1 et 6\n");
-        }
-    }
-    while(choix != 6);
-    printf("Au revoir !\n");
-    return EXIT_SUCCESS;
 }
+
